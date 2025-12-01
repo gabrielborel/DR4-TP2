1fc48eae30cbb90a63a83d68bd95ef65e61c0ca3
Gabriel Borel <biel_borel@hotmail.com>
2025-12-01 12:15:32 -0500

    Criação de um projeto Java com Spring Boot para o DR4-TP2

---
+++ src/main/java/al/infnet/edu/br/DR4_TP2/Dr4Tp2Application.java
@@ -0,0 +1,13 @@
+package al.infnet.edu.br.DR4_TP2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dr4Tp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dr4Tp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/al/infnet/edu/br/DR4_TP2/Dr4Tp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package al.infnet.edu.br.DR4_TP2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dr4Tp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
